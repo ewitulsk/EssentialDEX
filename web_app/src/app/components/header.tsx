ed829d1ef9edc3c974673a8154684b1293224d02
Martin <modo1997@hotmail.co.uk>
2024-10-20 01:43:52 -0700

    add logo

--- web_app/src/app/components/header.tsx
+++ web_app/src/app/components/header.tsx
@@ -1,12 +1,19 @@
 // web_app/src/app/components/Header.tsx
 import React from 'react';
 import { Link } from "react-router-dom";
+import Image from 'next/image';
+import logo from '../imgs/logo.png';
 
 const Header: React.FC = () => {
   return (
     <header className="z-10 bg-[#FE5C5C] flex h-full w-[50%] min-w-[18rem] max-w-[34.3125rem] items-center justify-between gap-[1rem] rounded-xl px-[0.74rem] py-8 xs:w-[38.125%] sm:min-w-[19rem] md:min-w-[28rem] text-black">
       <div className="flex items-center">
-        <span className="font-bold">ESSENTIAL</span>
+        <Image
+          src={logo}
+          alt="PintSwap Logo"
+          className="h-8 w-12 mr-2"
+        />
+        <span className="font-bold">PintSwap</span>
       </div>
       <div className="flex space-x-4">
         <Link to="/" className="flex items-center border-2 border-black px-4 py-2 rounded-full hover:bg-black hover:text-[#FE5C5C] transition-colors duration-300 hover:border-2 hover:border-black">
