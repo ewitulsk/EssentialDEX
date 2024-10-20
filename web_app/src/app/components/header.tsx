c49b0fb281cf82eb1aeb80978b99aba215dba236
Martin <modo1997@hotmail.co.uk>
2024-10-20 05:06:37 -0700

    hover color

--- web_app/src/app/components/header.tsx
+++ web_app/src/app/components/header.tsx
@@ -16,7 +16,7 @@ const Header: React.FC<{ color: string }> = ({ color }) => {
         <span className="font-bold text-sm leading-5" style={{ fontWeight: 600 }}>PINTSWAP</span>
       </div>
       <div className="flex space-x-4">
-        <Link to="/" className={"flex items-center border-2 border-black px-4 py-2 rounded-full hover:bg-black hover:text-[${color}] transition-colors duration-300 hover:border-2 hover:border-black font-semibold text-sm leading-5"}>
+        <Link to="/" className={"flex items-center border-2 border-black px-4 py-2 rounded-full hover:bg-black hover:text-[" + color + "] transition-colors duration-300 hover:border-2 hover:border-black font-semibold text-sm leading-5"}>
           SWAP
         </Link>
         <Link to="/pool" className={`flex items-center bg-black text-[${color}] px-4 py-2 rounded-full border-2 border-black hover:bg-transparent hover:text-black transition-colors duration-300 font-semibold text-sm leading-5`}>
