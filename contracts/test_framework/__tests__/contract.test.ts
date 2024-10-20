f5f95b7e2000169cd120405c5d8390d59120d6e0
Evan Witulski <ewitulsk@calpoly.edu>
2024-10-20 07:10:30 -0700

    swap works?

--- contracts/test_framework/__tests__/contract.test.ts
+++ contracts/test_framework/__tests__/contract.test.ts
@@ -8,11 +8,11 @@ import { createHash } from 'crypto';
 
 const LOCAL_SERVER = "https://bigbangblock.builders"
 
-const CONTRACT = "F790ACCA0B90D4E33288429D7539CE542E10F75D0A2197BE5F47CEFD9F420C91" 
+const CONTRACT = "CDE35CAE90D37B5E4C3E6EA6B0DDBD6AD65C1676451D33B36B058D8D35A20D9E" 
 const MINT_USDC_PRED = "5F3A37CDA561A8B40870DB8758711FD668E2FD9551453056B88031E2607EB463"
 const MINT_ESS_PRED = "AA1C297BFECED97CD4D3F69AEE869FABAE1764DCF7796B5C46B04CAC66682062"
 const ADD_LIQ_PRED = "A366B03643862475A71709883E0A3065E93A28CE23C44A3CA85CAB854E62D330"
-const SWAP_PRED = "396A67524E015070AD12203646B44DC39FA761A82BB1C69DCF61CBBD7E953DC9"
+const SWAP_PRED = "03EF6300662CF00DB3C486849984E5E06AF20F4200400C005517ED371165AA85"
 
 
 // const ADD_LIQUIDITY_PREDICATE = "BD77574D5A00D8717E0726F33EB6922B5F0189D7AC4EED7BB336B8F9A483F776"
@@ -181,8 +181,6 @@ async function provide_liquidity(wallet: number, usdc_in: number, ess_in: number
         predicate:  ADD_LIQ_PRED
     } as PredicateAddress
 
-
-
     const usdc_before_raw = (await essential.queryState(CONTRACT, ["0000000000000000"]))
     let reserve_usdc_before = 0;
     if(usdc_before_raw != null){
@@ -299,44 +297,61 @@ async function provide_liquidity(wallet: number, usdc_in: number, ess_in: number
     return lp_balance_after - lp_balance_before
 }
 
-test("provide liq from func", async () => {
-    let wallet = 1;
-    let usdc_amount = 1000;
-    let ess_amount = 40000;
+// which_token 1 for USDC, 0 for Ess
+async function calc_out_from_in(which_token: number, input_amount: number){
+    //STATE CHANGES
+    let reserve_usdc = await get_reserve_usdc()
+    let reserve_ess = await get_reserve_ess()
+
+    //More Decision Declaration
+    let amount_out = 0;
+    if (which_token == 1){
+        amount_out = Math.floor((input_amount * reserve_ess) / (reserve_usdc + input_amount))
+    }
+    else{
+        amount_out = Math.floor((input_amount * reserve_usdc) / (reserve_ess + input_amount))
+    }
+    return amount_out;
+}
+
+// test("provide liq from func", async () => {
+//     let wallet = 1;
+//     let usdc_amount = 1000;
+//     let ess_amount = 40000;
 
 
-    let usdc_minted = await mint_usdc(wallet, usdc_amount)
-    let ess_minted = await mint_ess(wallet, ess_amount)
+//     let usdc_minted = await mint_usdc(wallet, usdc_amount)
+//     let ess_minted = await mint_ess(wallet, ess_amount)
 
-    let usdc_reserves = await get_reserve_usdc();
-    let ess_reserves = await get_reserve_ess();
+//     let usdc_reserves = await get_reserve_usdc();
+//     let ess_reserves = await get_reserve_ess();
 
-    console.log("Ess: "+ess_minted);
-    console.log("USDC:: "+usdc_minted);
+//     console.log("Ess: "+ess_minted);
+//     console.log("USDC:: "+usdc_minted);
 
-    console.log("USDC Reserves: "+usdc_reserves);
-    console.log("Ess Reserves: "+ess_reserves);
+//     console.log("USDC Reserves: "+usdc_reserves);
+//     console.log("Ess Reserves: "+ess_reserves);
 
-    let provided = await provide_liquidity(wallet, usdc_minted, ess_minted)
+//     let provided = await provide_liquidity(wallet, usdc_minted, ess_minted)
 
-    let real = await lp_bal(wallet);
+//     let real = await lp_bal(wallet);
 
-    let usdc_bal_ = await usdc_bal(wallet)
-    let ess_bal_ = await ess_bal(wallet)
+//     let usdc_bal_ = await usdc_bal(wallet)
+//     let ess_bal_ = await ess_bal(wallet)
 
-    usdc_reserves = await get_reserve_usdc();
-    ess_reserves = await get_reserve_ess();
+//     usdc_reserves = await get_reserve_usdc();
+//     ess_reserves = await get_reserve_ess();
 
-    console.log("USDC Reserves: "+usdc_reserves);
-    console.log("Ess Reserves: "+ess_reserves);
+//     console.log("USDC Reserves: "+usdc_reserves);
+//     console.log("Ess Reserves: "+ess_reserves);
 
-    console.log("Ess: "+usdc_bal_);
-    console.log("USDC:: "+ess_bal_);
+//     console.log("Ess: "+usdc_bal_);
+//     console.log("USDC:: "+ess_bal_);
 
-    console.log("LP Bal: "+real);
+//     console.log("LP Bal: "+real);
 
-    expect(provided).toBe(real);
-})
+//     expect(provided).toBe(real);
+// })
 
 
 // test('test mint from func', async () => {
@@ -606,11 +621,140 @@ test("provide liq from func", async () => {
 
 // }, 1000000);
 
+// which_token: 1 for USDC, 0 for Ess  
+async function swap(wallet: number, which_token: number){
+    let essential = new EssentialClient(LOCAL_SERVER);
+    
+    let swapAddrs = {
+        contract: CONTRACT,
+        predicate:  SWAP_PRED
+    } as PredicateAddress
 
+    //Ensure there is liquidity in the pool
+    let usdc_amount = 1000;
+    let ess_amount = 40000;
+    let usdc_minted = await mint_usdc(wallet, usdc_amount)
+    let ess_minted = await mint_ess(wallet, ess_amount)
+    let usdc_reserves = await get_reserve_usdc();
+    let ess_reserves = await get_reserve_ess();
+    let provided = await provide_liquidity(wallet, usdc_minted, ess_minted)
+
+    //Ensure user has token
+    await mint_usdc(wallet, usdc_amount)
+    await mint_ess(wallet, ess_amount)
+
+    //DECISION & STATE
+    let user = wallet;
+    let amount_in = 30;
+    let token_in = which_token; // 1 for USDC, 0 for Ess
+    let min_amount_out = 0;
+
+
+    //STATE CHANGES
+    let reserve_usdc = await get_reserve_usdc()
+    let reserve_ess = await get_reserve_ess()
+    let user_usdc_bal = await usdc_bal(wallet);
+    let user_ess_bal = await ess_bal(wallet);
+
+    //More Decision Declaration
+    let amount_out = await calc_out_from_in(token_in, amount_in)
+    // constraint amount_out == cond {
+    //     token_in => (amount_in * reserve_ess) / (reserve_usdc + amount_in),
+    //     else => (amount_in * reserve_usdc) / (reserve_ess + amount_in),
+    // };
+
+    //DESCISION VARIABLES
+    let decision_variables = [
+        [user],
+        [amount_in],
+        [token_in],
+        [min_amount_out],
+        [amount_out]
+    ]
+
+    let new_reserve_usdc = token_in ? reserve_usdc + amount_in : reserve_usdc - amount_out
+    let new_reserve_ess = token_in ? reserve_ess - amount_out : reserve_ess + amount_in
+    let new_user_usdc_bal = token_in ? user_usdc_bal - amount_in : user_usdc_bal + amount_out
+    let new_user_ess_bal = token_in ? user_ess_bal  + amount_out : user_ess_bal - amount_in
+
+    let stateMuts = [
+        {
+            key: [0],
+            value: [new_reserve_usdc]
+        },
+        {
+            key: [1],
+            value: [new_reserve_ess]
+        },
+        {
+            key: [5, user],
+            value: [new_user_usdc_bal]
+        },
+        {
+            key: [6, user],
+            value: [new_user_ess_bal]
+        }  
+    ] as Mutation[]
+    
+    let solutionData = {
+        predicate_to_solve: swapAddrs,
+        decision_variables: decision_variables,
+        transient_data: [],
+        state_mutations: stateMuts
+    } as SolutionData
+
+    let solution = new Solution([solutionData])
+    let tx_hash = await essential.submitSolution(solution);
+    console.log(tx_hash)
+
+    await sleep(8000);
+
+}
+
+test('test swap from func', async () => {
+    let wallet = 1;
+    let which_token = 1
+    let amount_in = 3
+
+    //Ensure there is liquidity in the pool
+    let usdc_amount = 1000;
+    let ess_amount = 40000;
+    let usdc_minted = await mint_usdc(wallet, usdc_amount)
+    let ess_minted = await mint_ess(wallet, ess_amount)
+    let provided = await provide_liquidity(wallet, usdc_minted, ess_minted)
+
+    //Ensure user has token
+    await mint_usdc(wallet, usdc_amount)
+    await mint_ess(wallet, ess_amount)
+
+    let reserve_usdc = await get_reserve_usdc()
+    let reserve_ess = await get_reserve_ess()
+    let user_usdc_bal = await usdc_bal(wallet);
+    let user_ess_bal = await ess_bal(wallet);
+
+    //More Decision Declaration
+    let amount_out = await calc_out_from_in(which_token, amount_in)
+
+    let new_reserve_usdc = which_token ? reserve_usdc + amount_in : reserve_usdc - amount_out
+    let new_reserve_ess = which_token ? reserve_ess - amount_out : reserve_ess + amount_in
+    let new_user_usdc_bal = which_token ? user_usdc_bal - amount_in : user_usdc_bal + amount_out
+    let new_user_ess_bal = which_token ? user_ess_bal  + amount_out : user_ess_bal - amount_in
+
+    await swap(wallet, 1)
+
+    const validated_user_usdc_bal = await usdc_bal(wallet);
+
+    console.log("Gotten: "+validated_user_usdc_bal);
+    console.log("Set: "+new_user_usdc_bal);
+
+    expect(validated_user_usdc_bal).toBe(new_user_usdc_bal);
+
+}, 1000000);
 
 
 // test('test swap!!!', async () => {
 //     let essential = new EssentialClient(LOCAL_SERVER);
+    
 
 //     let swapAddrs = {
 //         contract: CONTRACT,
@@ -618,6 +762,20 @@ test("provide liq from func", async () => {
 //     } as PredicateAddress
 
 //     let wallet = 1 //Wallet 0
+
+
+//     //Ensure there is liquidity in the pool
+//     let usdc_amount = 1000;
+//     let ess_amount = 40000;
+//     let usdc_minted = await mint_usdc(wallet, usdc_amount)
+//     let ess_minted = await mint_ess(wallet, ess_amount)
+//     let usdc_reserves = await get_reserve_usdc();
+//     let ess_reserves = await get_reserve_ess();
+//     let provided = await provide_liquidity(wallet, usdc_minted, ess_minted)
+
+//     //Ensure user has token
+//     await mint_usdc(wallet, usdc_amount)
+//     await mint_ess(wallet, ess_amount)
 
 //     //DECISION & STATE
 //     let user = wallet;
@@ -627,20 +785,22 @@ test("provide liq from func", async () => {
 
 
 //     //STATE CHANGES
-//     let reserve_usdc = await get_reserve_usdc() + 400
+//     let reserve_usdc = await get_reserve_usdc()
 //     let reserve_ess = await get_reserve_ess()
 //     let user_usdc_bal = await usdc_bal(wallet);
 //     let user_ess_bal = await ess_bal(wallet);
 
+//     console.log("Incoming State: ")
+//     console.log("USDC Reserves: "+reserve_usdc)
+//     console.log("Ess Reserves: "+reserve_ess)
+//     console.log("USDC Bal: "+user_usdc_bal)
+//     console.log("Ess Bal: "+user_ess_bal)
 
 //     //More Decision Declaration
-//     let amount_out = 0;
-//     if (Math.floor((amount_in * reserve_ess) / (reserve_usdc + amount_in))){
-//         amount_out = token_in
-//     }
-//     else{
-//         amount_out = (amount_in * reserve_usdc) / (reserve_ess + amount_in)
-//     }
+//     let amount_out = await calc_out_from_in(token_in, amount_in)
+
+//     console.log("Amount In:"+amount_in);
+//     console.log("Amount Out:"+amount_out);
 //     // constraint amount_out == cond {
 //     //     token_in => (amount_in * reserve_ess) / (reserve_usdc + amount_in),
 //     //     else => (amount_in * reserve_usdc) / (reserve_ess + amount_in),
@@ -655,22 +815,27 @@ test("provide liq from func", async () => {
 //         [amount_out]
 //     ]
 
+//     let new_reserve_usdc = token_in ? reserve_usdc + amount_in : reserve_usdc - amount_out
+//     let new_reserve_ess = token_in ? reserve_ess - amount_out : reserve_ess + amount_in
+//     let new_user_usdc_bal = token_in ? user_usdc_bal - amount_in : user_usdc_bal + amount_out
+//     let new_user_ess_bal = token_in ? user_ess_bal  + amount_out : user_ess_bal - amount_in
+
 //     let stateMuts = [
 //         {
 //             key: [0],
-//             value: [reserve_usdc]
+//             value: [new_reserve_usdc]
 //         },
 //         {
 //             key: [1],
-//             value: [reserve_ess]
+//             value: [new_reserve_ess]
 //         },
 //         {
-//             key: [5, wallet],
-//             value: [user_usdc_bal]
+//             key: [5, user],
+//             value: [new_user_usdc_bal]
 //         },
 //         {
-//             key: [6, wallet],
-//             value: [user_ess_bal]
+//             key: [6, user],
+//             value: [new_user_ess_bal]
 //         }  
 //     ] as Mutation[]
     
@@ -693,8 +858,16 @@ test("provide liq from func", async () => {
 
 //     await sleep(8000)
 
-//     const reserve_usdc_after = await essential.queryState(CONTRACT, ["0000000000000000"])
-//     expect(reserve_usdc_after).toEqual([reserve_usdc])
+//     const validated_user_usdc_bal = await usdc_bal(user);
+
+//     console.log("Gotten: "+validated_user_usdc_bal);
+//     console.log("Set: "+new_user_usdc_bal);
+
+//     expect(validated_user_usdc_bal).toBe(new_user_usdc_bal);
+
+
+//     // const reserve_usdc_after = await essential.queryState(CONTRACT, ["0000000000000000"])
+//     // expect(reserve_usdc_after).toEqual([reserve_usdc])
     
 //     // const reserve_ess_after = await essential.queryState(CONTRACT, ["0000000000000001"])
 //     // expect(reserve_ess_after).toEqual([reserve_ess])
@@ -717,34 +890,34 @@ test("provide liq from func", async () => {
 // }, 1000000);
 
 
-// test("", () => {
-//     console.log("test");
-// })
+test("", () => {
+    console.log("test");
+})
 
-// // Function to convert a public key to an i64 (using BigInt)
-// function pubkeyToI64(pubkey: Buffer): bigint {
-//     // Hash the public key using SHA-256
-//     const hash = createHash('sha256');
-//     hash.update(pubkey);
-//     const result = hash.digest();
+// Function to convert a public key to an i64 (using BigInt)
+function pubkeyToI64(pubkey: Buffer): bigint {
+    // Hash the public key using SHA-256
+    const hash = createHash('sha256');
+    hash.update(pubkey);
+    const result = hash.digest();
 
-//     // Convert the first 8 bytes of the hash to a BigInt
-//     const bytes = result.slice(0, 8);
-//     let i64Value: bigint = BigInt(0);
+    // Convert the first 8 bytes of the hash to a BigInt
+    const bytes = result.slice(0, 8);
+    let i64Value: bigint = BigInt(0);
 
-//     for (let i = 0; i < bytes.length; i++) {
-//         i64Value |= BigInt(bytes[i]) << BigInt(i * 8);
-//     }
+    for (let i = 0; i < bytes.length; i++) {
+        i64Value |= BigInt(bytes[i]) << BigInt(i * 8);
+    }
 
-//     // If you want to treat it as a signed i64, apply the mask
-//     if (i64Value >= BigInt(2n ** BigInt(63))) {
-//         i64Value -= BigInt(2n ** BigInt(64)); // Convert to signed i64
-//     }
+    // If you want to treat it as a signed i64, apply the mask
+    if (i64Value >= BigInt(2n ** BigInt(63))) {
+        i64Value -= BigInt(2n ** BigInt(64)); // Convert to signed i64
+    }
 
-//     return i64Value;
-// }
+    return i64Value;
+}
 
-// function sleep(ms: number) {
-//     return new Promise(resolve => setTimeout(resolve, ms));
-// }
+function sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
